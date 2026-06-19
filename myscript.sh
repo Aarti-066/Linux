fa71861ddc9fc33bfb89dfc2d44837e989f7aa33
aartipariharcd ..cd ..git config --global user.name aartiparihargit config --global user.email aartiparihar671@gmail.com <aartiparihar671@gmail>
2026-06-19 11:07:08 +0000

    added system info script

---
+++ myscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "System Information Report"
+echo "========================="
+echo "Your username is: $(whoami)"
+echo "Today's date is: $(date)"
+echo "Your current location: $(pwd)"
+echo "Disk space available:"
+df -h
+echo "Memory available:"
+free -h
+echo "========================="
+echo "Report Complete!"#!/bin/bash
+echo "Hello! I am Aarti"
+echo "I am learning DevOps"
+echo "Today is my Day 3"

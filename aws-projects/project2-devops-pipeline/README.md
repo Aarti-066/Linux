d4353f33ee7161900bb49effe2462f640a51fa15
Aarti Parihar <aartiparihar671@gmail>
2026-06-25 09:23:35 +0000

    added capstone project 2 devops pipeline

---
+++ aws-projects/project2-devops-pipeline/README.md
@@ -0,0 +1,14 @@
+# Capstone Project 2 - DevOps CI/CD Pipeline
+
+## What I Built
+- Created 4 EC2 servers using Terraform
+- Worker1: Jenkins + Java 21
+- Worker2, Worker4: Docker + Kubernetes
+- Worker3: Java + Docker + Kubernetes
+- Jenkins CI/CD pipeline that auto builds Docker images
+
+## Tools Used
+Jenkins, Docker, Kubernetes, Terraform, AWS EC2, GitHub
+
+## Pipeline Result
+Build #4 — SUCCESS in 10 seconds!
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,11 +11,38 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "my_server" {
+resource "aws_instance" "worker1" {
   ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t3.micro"
-
+  key_name      = "abc-keypair"
   tags = {
-    Name = "devops-server-01"
+    Name = "Worker1-Jenkins"
+  }
+}
+
+resource "aws_instance" "worker2" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"
+  key_name      = "abc-keypair"
+  tags = {
+    Name = "Worker2-Docker-K8s"
+  }
+}
+
+resource "aws_instance" "worker3" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"
+  key_name      = "abc-keypair"
+  tags = {
+    Name = "Worker3-Java-Docker-K8s"
+  }
+}
+
+resource "aws_instance" "worker4" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"
+  key_name      = "abc-keypair"
+  tags = {
+    Name = "Worker4-Docker-K8s"
   }
 }
